f4e833893db7bf541b5c77ecf4d5026453277fcd
Shivanand <73015307+Shivanand1999@users.noreply.github.com>
2022-09-19 23:53:13 +0530

    Add files via upload

--- Step 1 Learn Basics/1.3 Basic Recursion/2_Print_1_To_N.cpp
+++ Step 1 Learn Basics/1.3 Basic Recursion/2_Print_1_To_N.cpp
@@ -8,6 +8,14 @@ void print_1_to_N(int i, int n){
     print_1_to_N(i+1,n);
 }
 
+//Backtracking:
+/*
+void print_1_to_N(int i, int n){
+    if(i < 1) return ;
+    print_1_to_N(i-1,n);
+    cout << i <<" ";
+}
+*/
 int main(){
     int n;
     cin >> n;
