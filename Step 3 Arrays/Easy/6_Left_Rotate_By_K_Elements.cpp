0b891fdb543e6c0be3bc941c93b63c007492cd6e
Shivanand <73015307+Shivanand1999@users.noreply.github.com>
2022-09-24 17:02:03 +0530

    Add files via upload

--- Step 3 Arrays/Easy/6_Left_Rotate_By_K_Elements.cpp
+++ Step 3 Arrays/Easy/6_Left_Rotate_By_K_Elements.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Efficient Solution: T = O(n), S = O(1)
-void leftRotate(int arr[], int n, int k){
-    Reverse(arr, 0, k-1);
-    Reverse(arr, k, n-1);
-    Reverse(arr, 0, n-1);
-}
-
 void Reverse(int arr[], int start, int end){
         while(start<=end){
             swap(arr[start], arr[end]);
             start++;
             end--;
         }
-    }
+}
+
+//Efficient Solution: T = O(n), S = O(1)
+void leftRotate(int arr[], int n, int k){
+    Reverse(arr, 0, k-1);
+    Reverse(arr, k, n-1);
+    Reverse(arr, 0, n-1);
+}
 
 int main(){
     int n;
@@ -23,5 +23,8 @@ int main(){
     for(int i=0; i<n; i++) cin >> arr[i];
     int k;
     cin >> k;
+    for(int i=0; i<n; i++) cout << arr[i]<<" ";
     leftRotate(arr, n, k);
+    cout << endl;
+    for(int i=0; i<n; i++) cout << arr[i]<<" ";
 }
